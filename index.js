7c0d3ff6a64f5aee95fc33d0f07f4242b08df7ec
Isaac I. Martinez M <isaacismaelx14@gmail.com>
2021-11-14 20:53:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 4100;
+const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
